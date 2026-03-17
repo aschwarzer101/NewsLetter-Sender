f565aa7d8d7ae8d23a7873d0798287f0877a7b34
aschwarzer101 <124056235+aschwarzer101@users.noreply.github.com>
2026-03-17 15:49:20 -0400

    adding in SQL framework

---
+++ database_files/newsletter_data.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS NewsLetterSchools; 
+CREATE TABLE NewsLetterSchools
+(
+    schoolID INT PRIMARY KEY AUTO_INCREMENT, 
+    schoolName VARCHAR(255) NOT NULL, 
+    town VARCHAR(255) NOT NULL, 
+
+
+)
+
+DROP TABLE IF EXISTS Newsletters; 
+CREATE TABLE Newsletters
+(
+    schoolID INT,
+    nlID INT NOT NULL AUTO_INCREMENT, 
+    newsletterName VARCHAR(255) NOT NULL, 
+    FOREIGN KEY (schoolID) REFERENCES NewsLetterSchools (schoolID)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+    PRIMARY KEY (schoolID, nlID)
+
+)
