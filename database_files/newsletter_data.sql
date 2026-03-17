e0f1f2fb526fa8e23f0be72fcab386a4224514b2
aschwarzer101 <124056235+aschwarzer101@users.noreply.github.com>
2026-03-17 15:57:27 -0400

    addin in newslsubmision tables

--- database_files/newsletter_data.sql
+++ database_files/newsletter_data.sql
@@ -13,10 +13,24 @@ CREATE TABLE Newsletters
 (
     schoolID INT,
     nlID INT NOT NULL AUTO_INCREMENT, 
-    newsletterName VARCHAR(255) NOT NULL, 
+    newsletterName VARCHAR(255) UNIQUE NOT NULL,
+    publishDate VARCHAR(255) NOT NULL,
+    recipientEmail VARCHAR(255) NOT NULL,
     FOREIGN KEY (schoolID) REFERENCES NewsLetterSchools (schoolID)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+        ON DELETE RESTRICT,
     PRIMARY KEY (schoolID, nlID)
 
 );
+
+DROP TABLE IF EXISS NWSLSubmission; 
+CREATE TABLE NWSLSubmission(
+    nlID INT NOT NULL, 
+    submissionID INT NOT NULL, 
+    submissionDate DATETIME NOT NULL, 
+    memoText TEXT NOT NULL, 
+    FOREIGN KEY (nlID) REFERENCES Newsletters (nlID)
+        ON UPDATE RESTRICT
+        ON DELETE RESTRICT
+    PRIMARY KEY (nlID, submissionID)
+); 
